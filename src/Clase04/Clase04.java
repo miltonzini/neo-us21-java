66f41bd09b608f9d358d89890150f90f1dd75b63
miltonzini <miltonzini@gmail.com>
2023-11-21 23:22:42 -0300

    Create Clase04.java

---
+++ src/Clase04/Clase04.java
@@ -0,0 +1,43 @@
+package Clase04;
+
+import javax.swing.JOptionPane;
+
+public class Clase04 {
+    public static void main(String[] args) {
+        
+        
+        String oracion = "Cohorte uno se egresa en: ";
+        
+        int edad = 2024;
+        
+        String resultado = oracion + edad;
+        
+        JOptionPane.showMessageDialog(null, resultado);
+        
+        
+        // int edad = 30;
+        
+        // int edadProxima = 31;
+        
+        // int resultado = edad + edadProxima;
+        
+        // JOptionPane.showMessageDialog(null, resultado);
+        
+        
+        // int numeroUno = 10;
+        
+        // int numeroDos = 0;
+        
+        // int resultado = numeroUno / numeroDos;
+        
+        // System.out.println(resultado);
+        
+        
+        
+        /*
+        let numero = "texto";       
+        */
+        
+    }
+
+}
