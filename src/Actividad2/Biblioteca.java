5b780566e79a221a861da99b2529cae86ff72627
miltonzini <miltonzini@gmail.com>
2023-11-28 17:05:08 -0300

    create listadoUsuarios  imprimirListadoUsuarios()
    -create List
    -populate with sample users
    -create imprimirListadoUsuarios method

--- src/Actividad2/Biblioteca.java
+++ src/Actividad2/Biblioteca.java
@@ -1,10 +1,14 @@
 package Actividad2;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+    private static ArrayList<Usuario> listadoUsuarios = new ArrayList<>();
+
     public static void precargarUsuarios() {
-        System.out.println("se precargan los usuarios"); // TODO
-        // crear lista para usuarios
-        // instanciar Usuario
+        // precargar usuarios de ejemplo
+        listadoUsuarios.add(new Usuario("20983245", "Carlos", "Gomez", "carlos@ejemplo.com", "carlos123", false));
+        listadoUsuarios.add(new Usuario("38123546", "Ana", "Díaz", "anadiaz@ejemplo.com", "conejo", false));
     }
 
     public static void precargarLibros() {
@@ -12,6 +16,18 @@ public class Biblioteca {
         // crear lista para libros
         // instanciar Libro varias veces
     }
+
+
+
+    public static void imprimirListadoUsuarios() { // TODO: convertir a getter
+        System.out.println("--------------------------"); 
+        System.out.println("Listado Usuarios Registrados:");
+        for (Usuario usuario : listadoUsuarios) {
+            System.out.println(usuario.getNombre() + " " + usuario.getApellido());
+        }
+        System.out.println("--------------------------");
+    }
+
 
 
 }
