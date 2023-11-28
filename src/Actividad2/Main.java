b79827e6b5ae7ef87bd60e2b2173e49e84cc7a1c
miltonzini <miltonzini@gmail.com>
2023-11-28 18:51:26 -0300

    Update Main.java
    create basic functions workflow

--- src/Actividad2/Main.java
+++ src/Actividad2/Main.java
@@ -1,10 +1,23 @@
 package Actividad2;
 
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         // acceder a métodos iniciales de biblioteca
         Biblioteca.precargarUsuarios();
         Biblioteca.precargarLibros();
+        // lógica de registro de usuario
+        // ... 
+
+        // una vez terminado dirigiar a Menu Principal
+        menuPrincipal();
+    }
+
+    
+    
+    public static void menuPrincipal() {
+        
     }
 }
 
